e81d9d461600d5829e1dcd832a659121dcff1c61
Nora Valletta <nora.valletta@gmail.com>
2015-08-25 09:13:00 -0600

    Fixes an irritating bug which broke Cat and Human imports

--- PythonDesignPatterns/python_design_patterns.py
+++ PythonDesignPatterns/python_design_patterns.py
@@ -1,10 +1,8 @@
 ﻿import sys
 from behavioral.memento.memento_pattern import MementoPattern
 from behavioral.observer.observer_pattern import ObserverPattern
-from builtins import print
 
 def main():
-    print("It works!")
     pattern = ObserverPattern()
     pattern.demonstrate()
 
