a0cb992ca862560de7163973f031d905e46ef185
Nora Valletta <nora.valletta@gmail.com>
2015-09-08 13:46:06 -0600

    Guarded suspension is working!... I think. Must test.

---
+++ PythonDesignPatterns/concurrency/guarded_suspension/coach.py
@@ -0,0 +1,29 @@
+﻿import sched, time
+import sys
+
+class Coach(object):
+
+    _play_ball = False
+
+    @staticmethod
+    def game_is_on():
+        return Coach._play_ball
+
+    @staticmethod
+    def toggle_game_play():
+        if Coach._play_ball:
+            Coach._play_ball = False
+            print("Stop playing.")
+        else:
+            Coach._play_ball = True
+            print("If you can dodge a wrench, you can dodge a ball! Game on!")
+        sys.stdout.flush()
+
+    def __init__(self):
+        Coach._play_ball = False
+
+    @staticmethod
+    def coach_the_game():
+        s = sched.scheduler(time.time, time.sleep)
+        s.enter(5, 1, Coach.toggle_game_play)
+        s.run()
---
+++ PythonDesignPatterns/concurrency/guarded_suspension/player.py
@@ -0,0 +1,23 @@
+﻿from concurrency.guarded_suspension.coach import Coach
+import threading
+import sys
+
+class Player(object):
+    """Likes to play dodge ball."""
+
+    def __init__(self, name):
+        self.name = name
+
+    def throw_balls(self):
+        while Coach.game_is_on():
+            print(self.name + " throws a ball!")
+
+    def start_playing(self):
+        while True:
+            self.throw_balls()
+            sys.stdout.flush()
+
+    def participate(self):
+        t = threading.Thread(target=self.start_playing)
+        t.start()
+        
