a3bf920b935822d96316ef5363b1590591bab125
Nora Valletta <nora.valletta@gmail.com>
2015-09-09 08:27:52 -0600

    Adds threadpool folder and basic files

---
+++ PythonDesignPatterns/concurrency/thread_pool/person.py
@@ -0,0 +1,11 @@
+﻿import sys
+
+class Person(object):
+    """Hi, I'm a person."""
+
+    def __init__(self, name):
+        self.name = name
+
+    def have_fun(self):
+        print(self.name + " is having fun!")
+        sys.stdout.flush()
