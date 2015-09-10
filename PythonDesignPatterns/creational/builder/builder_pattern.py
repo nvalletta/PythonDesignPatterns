9e5a62c096adf6373aef580ced8e634346f1066d
Nora Valletta <nora.valletta@gmail.com>
2015-09-10 09:23:32 -0600

    Builder done!

---
+++ PythonDesignPatterns/creational/builder/builder_pattern.py
@@ -0,0 +1,12 @@
+﻿from creational.builder.grooming_appointment import GroomingAppointment
+
+class BuilderPattern(object):
+    """description of class"""
+
+    def __init__(self):
+        self.appointment_one = GroomingAppointment("11/01/2015", "435-123-4567", "Shawn", "123 Awesome Street", "", "", 10, "big", True)
+        self.appointment_two = GroomingAppointment("01/20/2016", "808-572-2472", "Nora", "", "Derps", "Dragon", 800, "lol it's huge", False, True)
+
+    def demonstrate(self):
+        print(self.appointment_one.print_appointment_details())
+        print(self.appointment_two.print_appointment_details())
---
+++ PythonDesignPatterns/creational/builder/grooming_appointment.py
@@ -0,0 +1,51 @@
+﻿class GroomingAppointment(object):
+    """description of class"""
+
+    def __init__(self, 
+                 appointment_date, 
+                 phone_number, 
+                 customer_name,
+                 address="",
+                 dog_name="",
+                 dog_breed="",
+                 dog_age="",
+                 dog_size="",
+                 bath=False,
+                 nails=False,
+                 ears=False,
+                 hair_cut=False):
+
+        self.appointment_date = appointment_date
+        self.phone_number = phone_number
+        self.customer_name = customer_name
+
+        self.address = address
+
+        self.dog_name = dog_name
+        self.dog_breed = dog_breed
+        self.dog_age = dog_age
+        self.dog_size = dog_size
+
+        self.bath = False
+        self.nails = False
+        self.ears = False
+        self.hair_cut = False
+
+
+    def print_appointment_details(self):
+        message = """
+            Date: {}
+            Phone Number: {}
+            Customer Name: {}
+            Address: {}
+            Dog Name: {}
+            Dog Breed: {}
+            Dog Age: {}
+            Dog Size: {}
+            Bath: {}
+            Nails: {}
+            Ears: {}
+            Hair Cut: {}
+            """.format(self.appointment_date, self.phone_number, self.customer_name, self.address, self.dog_name, self.dog_breed, self.dog_age, self.dog_size, self.bath, self.nails, self.ears, self.hair_cut)
+
+        print(message)
